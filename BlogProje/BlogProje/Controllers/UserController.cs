ead8479e7897ee09b82a958fbc5be8aada3955a6
Ali Öztürk <84031734+traaz@users.noreply.github.com>
2022-12-26 20:45:01 +0300

    Yazar Paneli Blog Listeleme

--- BlogProje/BlogProje/Controllers/UserController.cs
+++ BlogProje/BlogProje/Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete;
+using Entity.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,8 @@ namespace BlogProje.Controllers
     public class UserController : Controller
     {
         // GET: AuthorLogin
+        BlogManager bm = new BlogManager();
+
         UserProfileManager userProfile = new UserProfileManager();
         public ActionResult Index()
         {
@@ -22,6 +26,100 @@ namespace BlogProje.Controllers
             var profile = userProfile.GetAuthorByMail(mail);
             return PartialView(profile);
 
+        }
+        public ActionResult BlogList(string mail)
+        {
+            mail = (string)Session["AuthorMail"];
+
+            Context c = new Context();
+            int id =c.Authors.Where(x=>x.AuthorMail == mail).Select(y=>y.AuthorId).FirstOrDefault();
+            var blogs = userProfile.GetBlogByAuthor(id);
+            return View(blogs);
+        }
+
+        [HttpGet]
+        public ActionResult updateBlog(int id)
+        {
+            Blog blog = bm.findBlog(id);
+            DataAccess.Concrete.Context c = new DataAccess.Concrete.Context();
+            List<SelectListItem> values = (from x in c.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+
+                                           }).ToList();
+            List<SelectListItem> values2 = (from x in c.Authors.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.AuthorName,
+                                                Value = x.AuthorId.ToString()
+
+                                            }).ToList();
+
+            ViewBag.values = values;
+            ViewBag.values2 = values2;
+            return View(blog);
+        }
+        [HttpPost]
+        public ActionResult updateBlog(Blog b)
+        {
+            bm.updateBlog(b);
+            DataAccess.Concrete.Context c = new DataAccess.Concrete.Context();
+            List<SelectListItem> values = (from x in c.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+
+                                           }).ToList();
+            List<SelectListItem> values2 = (from x in c.Authors.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.AuthorName,
+                                                Value = x.AuthorId.ToString()
+
+                                            }).ToList();
+
+            ViewBag.values = values;
+            ViewBag.values2 = values2;
+
+            return RedirectToAction("BlogList");
+        }
+
+        [HttpGet]
+        public ActionResult AddBlog()
+        {
+            DataAccess.Concrete.Context c = new DataAccess.Concrete.Context();
+            List<SelectListItem> values = (from x in c.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+
+                                           }).ToList();
+            List<SelectListItem> values2 = (from x in c.Authors.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.AuthorName,
+                                                Value = x.AuthorId.ToString()
+
+                                            }).ToList();
+
+            ViewBag.values = values;
+            ViewBag.values2 = values2;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddBlog(Blog b)
+        {
+            bm.AddBlog(b);
+            return RedirectToAction("BlogList");
+        }
+        public ActionResult deleteBlog(int id)
+        {
+            bm.DeleteBlog(id);
+            return RedirectToAction("BlogList");
         }
     }
 }
--- BlogProje/Business/Concrete/UserProfileManager.cs
+++ BlogProje/Business/Concrete/UserProfileManager.cs
@@ -11,9 +11,15 @@ namespace Business.Concrete
     public class UserProfileManager
     {
         Repository<Author> repoAuthor = new Repository<Author>();
+        Repository<Blog> repoUser = new Repository<Blog>();
+
         public List<Author> GetAuthorByMail(string mail)
         {
             return repoAuthor.List(x=>x.AuthorMail == mail);
+        }
+        public List<Blog> GetBlogByAuthor(int id)
+        {
+            return repoUser.List(x => x.AuthorId == id);
         }
     }
 }
